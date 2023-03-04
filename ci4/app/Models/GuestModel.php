273d775c8045d9b70fabf753787a599d1daf4824
Yanskitodo <120081076+Yanskitodo@users.noreply.github.com>
2023-03-04 09:32:31 +0800

    guest

---
+++ ci4/app/Models/GuestModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class GuestModel extends Model
+{
+    protected $table = 'eplascano_guests';
+
+    protected $allowedFields = ['name', 'email', 'website', 'comment', 'gender'];
+
+     public function getGuest()
+     {
+        return $this->findAll();
+     }
+}
