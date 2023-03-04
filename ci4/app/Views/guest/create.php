5187d9bfeb5cf925c9ea58e0bbc3ffbffe04fb78
Yanskitodo <120081076+Yanskitodo@users.noreply.github.com>
2023-03-04 16:40:20 +0800

    Update create.php

--- ci4/app/Views/guest/create.php
+++ ci4/app/Views/guest/create.php
@@ -6,13 +6,25 @@
 <form action="create" method="post">
     <?= csrf_field() ?>
 
-    <label for="title">Title</label>
-    <input type="input" name="title" value="<?= set_value('title') ?>">
+    <label for="name">Name</label>
+    <input type="input" name="name" value="<?= set_value('name') ?>">
     <br>
 
-    <label for="body">Text</label>
-    <textarea name="body" cols="45" rows="4"><?= set_value('body') ?></textarea>
+    <label for="email">Email</label>
+    <input type="input" name="email" value="<?= set_value('email') ?>">
     <br>
 
-    <input type="submit" name="submit" value="Create news item">
+    <label for="website">Website</label>
+    <input type="input" name="website" value="<?= set_value('website') ?>">
+    <br>
+
+    <label for="comment">Comment</label>
+    <textarea name="comment" cols="45" rows="4"><?= set_value('comment') ?></textarea>
+    <br>
+
+    <label for="gender">Gender</label>
+    <input type="input" name="gender" value="<?= set_value('gender') ?>">
+    <br>
+
+    <input type="submit" name="submit" value="Create guest entry">
 </form>
